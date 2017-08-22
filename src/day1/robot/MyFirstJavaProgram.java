79d2b30817903767b9f4517c3ae1db26ee6250ed
nealparth <league@WTS22.attlocal.net>
2017-08-22 11:56:18 -0700

    Workshop Day 2

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -1,15 +1,27 @@
 package day1.robot;
 
 import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		// START HERE
+		System.out.println("Hello world!");
+		Robot bot = new Robot("mini");
+		bot.setWindowColor(Color.blue);
+		bot.penDown();
+		bot.move(70);
+		bot.setPenColor(0, 255, 0);
+		bot.turn(90);
+		bot.setPenColor(700, 800, 200);
+		bot.move(70);
+		bot.turn(90);
+		bot.setPenColor(2, 600, 2);
+		bot.move(70);
+		bot.turn(90);
+		bot.setPenColor(300, 500, 4);
+		bot.move(70);
 	}
 }
--- src/day1/robot/TriangleShell.java
+++ src/day1/robot/TriangleShell.java
@@ -5,35 +5,44 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
+	Robot bot = new Robot("mini");
 
-	
 	void go() {
-		drawTriangle(100); // 3. delete this line (used only for testing)
 
 		// 6. Make the robot go as fast as possible
 
-		// 4. make a variable to hold the length of the triangle and set it to 50
+		// 4. make a variable to hold the length of the triangle and set it to
+		// 50
 
 		// 7. Do the following (up to step 10) 60 times
 
-			// 9. Change the color of the pen to a random color
-	
-			// 8. Increase the length of the side by 10 pixels
-	
-			// 5. call your drawTriangle() method using your length variable
-	
-			// 10. Turn the tortoise 6 degrees to the right
+		// 9. Change the color of the pen to a random color
+
+		// 8. Increase the length of the side by 10 pixels
+
+		// 5. call your drawTriangle() method using your length variable
+
+		// 10. Turn the tortoise 6 degrees to the right
 
 	}
 
-	/* 2. fill in the method below to draw a triangle. Use the length variable for the size of the triangle. */
+	/*
+	 * 2. fill in the method below to draw a triangle. Use the length variable
+	 * for the size of the triangle.
+	 */
 	private void drawTriangle(int length) {
-		
-		
-		
+		bot.setSpeed(10);
+		bot.setAngle(30);
+		bot.penDown();
+		bot.setPenColor(0, 255, 0);
+		bot.move(length);
+		bot.turn(120);
+		bot.move(length);
+		bot.turn(120);
+		bot.move(length);
+
 	}
 
-	
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
